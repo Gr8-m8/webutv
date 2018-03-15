f37a9919b73b090f51e9e8ffdbed8ce1a634ac12
Gr8-m8 <martinn.lindbladd@gmail.com>
2018-03-15 13:35:07 +0100

    playgame ratio, js

--- webutv/webutv/js/main.js
+++ webutv/webutv/js/main.js
@@ -1,1 +1,19 @@
-﻿ // Your code here!
+﻿class Game {
+	constructor() {
+		var name;
+		var src;
+	}
+}
+
+function resizeGame() {
+	document.getElementById("game").style.height = 9 * document.getElementById("game").scrollWidth / 20 + "px";
+	console.log(document.getElementById("game"));
+}
+
+window.onload = function () {
+	resizeGame();
+}
+
+window.onresize = function () {
+	resizeGame();
+}
