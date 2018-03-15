5f0d7fa23d2f33a0663626fb30ca22a0f651f4ed
Gr8-m8 <martinn.lindbladd@gmail.com>
2018-03-15 17:29:10 +0100

    js

--- webutv/webutv/js/main.js
+++ webutv/webutv/js/main.js
@@ -1,17 +1,51 @@
-﻿class Game {
-	constructor() {
-		var name;
-		var src;
+﻿var games = [];
+
+class Game {
+	constructor(setname = "test", setsrc = "", settags = "fetured") {
+		var name = setname;
+		var src = setsrc;
+		var tags = [];
+		tags.push(settags);
+	}
+
+	displayGame() {
+		return "test";
+	}
+}
+
+function initGames() {
+	games.push(
+		new Game("RickGame", "https://17mali.ssis.nu/RickGame/index.html", "fetured")
+	);
+
+	console.log(games[0].name);
+
+	displayGames();
+}
+
+function displayGames() {
+	for (var i = 0; i < games.length; i++) {
+		for (var t = 0; t < games[i].tags.length; t++) {
+			if (document.getElementById(games[i].tags[t]) != undefined || document.getElementById(games[i].tags[t]) != null) {
+				document.getElementById(games[i].tags[t]).innerHTML = games[i].displayGame();
+				console.log("SUCCSESS: " + games[i]);
+			} else {
+				console.log("ERROR: " + games[i]);
+			}
+		}
 	}
 }
 
 function resizeGame() {
-	document.getElementById("game").style.height = 9 * document.getElementById("game").scrollWidth / 20 + "px";
-	console.log(document.getElementById("game"));
+	if (document.getElementById("game") != null) {
+		document.getElementById("game").style.height = 9 * document.getElementById("game").scrollWidth / 20 + "px";
+		console.log(document.getElementById("game"));
+	}
 }
 
 window.onload = function () {
 	resizeGame();
+	initGames();
 }
 
 window.onresize = function () {
